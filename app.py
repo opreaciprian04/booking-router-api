e8b04d185e32e0b636c2d8ab2bc05e8ed1555d53
opreaciprian04 <opreaciprian04@gmail.com>
2026-04-28 03:54:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -197,6 +197,7 @@ def optimize():
                 "address": pick(r, ["address", "pickup_address"], ""),
                 "pickup_lat": pickup_lat,
                 "pickup_lng": pickup_lng,
+                "dropoff_address": dropoff_address
                 "dropoff_lat": dropoff_lat,
                 "dropoff_lng": dropoff_lng,
                 "seats": seats
